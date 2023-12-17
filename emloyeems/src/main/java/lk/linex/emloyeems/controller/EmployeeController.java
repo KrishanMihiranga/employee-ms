b5e72cbcd0992b80d51b52d314a0eea7bfbd86a1
Krishan <xp3cto95@gmail.com>
2023-12-17 12:50:29 +0530

    Update Employee using @PutMapping

--- emloyeems/src/main/java/lk/linex/emloyeems/controller/EmployeeController.java
+++ emloyeems/src/main/java/lk/linex/emloyeems/controller/EmployeeController.java
@@ -7,10 +7,7 @@ import lk.linex.emloyeems.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/employee")
@@ -47,5 +44,33 @@ public class EmployeeController {
                 responseDTO.setContent(null);
                 return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
             }
+    }
+
+    @PutMapping(value = "/updateEmployee")
+    public ResponseEntity updateEmployee(@RequestBody EmployeeDTO employeeDTO){
+        try {
+            String res = employeeService.updateEmployee(employeeDTO);
+            if (res.equals("00")){
+                responseDTO.setCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Success");
+                responseDTO.setContent(employeeDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+            }else if(res.equals("01")){
+                responseDTO.setCode(VarList.RSP_DUPLICATED);
+                responseDTO.setMessage("NOT A Registered Employee");
+                responseDTO.setContent(employeeDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            }else{
+                responseDTO.setCode(VarList.RSP_FAIL);
+                responseDTO.setMessage("Error");
+                responseDTO.setContent(null);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception e){
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(e.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- emloyeems/src/main/java/lk/linex/emloyeems/service/EmployeeService.java
+++ emloyeems/src/main/java/lk/linex/emloyeems/service/EmployeeService.java
@@ -25,4 +25,13 @@ public class EmployeeService {
             return VarList.RSP_SUCCESS;
         }
     }
+
+    public String updateEmployee(EmployeeDTO employeeDTO){
+        if (employeeRepo.existsById(employeeDTO.getEmpId())){
+            employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
+            return VarList.RSP_SUCCESS;
+        }else {
+            return VarList.RSP_NO_DATA_FOUND;
+        }
+    }
 }
