16ee7f571412a8c0cc0df34d11397cd5727b0356
Krishan <xp3cto95@gmail.com>
2023-12-17 13:07:03 +0530

    search employee

--- emloyeems/src/main/java/lk/linex/emloyeems/controller/EmployeeController.java
+++ emloyeems/src/main/java/lk/linex/emloyeems/controller/EmployeeController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/employee")
 public class EmployeeController {
@@ -63,6 +65,45 @@ public class EmployeeController {
             }else{
                 responseDTO.setCode(VarList.RSP_FAIL);
                 responseDTO.setMessage("Error");
+                responseDTO.setContent(null);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception e){
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(e.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/getAllEmployees")
+    public ResponseEntity getAllEmployees(){
+        try {
+            List<EmployeeDTO> employeeDTOList = employeeService.getAllEmployee();
+            responseDTO.setCode(VarList.RSP_SUCCESS);
+            responseDTO.setMessage("Success");
+            responseDTO.setContent(employeeDTOList);
+            return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+        }catch (Exception e){
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(e.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/searchEmployee/{empId}")
+    public ResponseEntity searchEmployee(@PathVariable int empId){
+        try {
+            EmployeeDTO employeeDTO = employeeService.searchEmployee(empId);
+            if (employeeDTO != null){
+                responseDTO.setCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Success");
+                responseDTO.setContent(employeeDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+            }else{
+                responseDTO.setCode(VarList.RSP_NO_DATA_FOUND);
+                responseDTO.setMessage("No Employee Available For this empId");
                 responseDTO.setContent(null);
                 return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
             }
--- emloyeems/src/main/java/lk/linex/emloyeems/service/EmployeeService.java
+++ emloyeems/src/main/java/lk/linex/emloyeems/service/EmployeeService.java
@@ -6,8 +6,12 @@ import lk.linex.emloyeems.entity.Employee;
 import lk.linex.emloyeems.repo.EmployeeRepo;
 import lk.linex.emloyeems.util.VarList;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -32,6 +36,20 @@ public class EmployeeService {
             return VarList.RSP_SUCCESS;
         }else {
             return VarList.RSP_NO_DATA_FOUND;
+        }
+    }
+
+    public List<EmployeeDTO> getAllEmployee(){
+        List<Employee> employeeList =  employeeRepo.findAll();
+        return modelMapper.map(employeeList, new TypeToken<ArrayList<EmployeeDTO>>(){}.getType());
+    }
+
+    public EmployeeDTO searchEmployee(int empId){
+        if(employeeRepo.existsById(empId)){
+            Employee employee = employeeRepo.findById(empId).orElse(null);
+            return modelMapper.map(employee, EmployeeDTO.class);
+        }else{
+            return null;
         }
     }
 }
