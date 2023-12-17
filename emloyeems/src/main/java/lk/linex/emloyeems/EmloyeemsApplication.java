07adf63bfde74538ac24e0d48794ed3ca67357b8
Krishan <xp3cto95@gmail.com>
2023-12-17 11:49:47 +0530

    init project

---
+++ emloyeems/src/main/java/lk/linex/emloyeems/EmloyeemsApplication.java
@@ -0,0 +1,13 @@
+package lk.linex.emloyeems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmloyeemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmloyeemsApplication.class, args);
+    }
+
+}
---
+++ emloyeems/src/test/java/lk/linex/emloyeems/EmloyeemsApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.linex.emloyeems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmloyeemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
